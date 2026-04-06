215339789473410f46afc43ec00d3b211970ac5c
Nho Luong <58627821+nholuongut@users.noreply.github.com>
2026-04-06 10:02:18 +0700

    Delete .github directory

--- .github/configs/renovate-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    platform: 'github',
-    // This ensures that the gitAuthor and gitSignOff fields match
-    gitAuthor: 'argoproj-renovate[bot] <161757507+argoproj-renovate[bot]@users.noreply.github.com>',
-    autodiscover: false,
-    allowPostUpgradeCommandTemplating: true,
-    allowedPostUpgradeCommands: [".*"],
-  };
